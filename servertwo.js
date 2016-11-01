8dead5de4682cfeea288f97cd0685843d1c4c190
Yosuke Hishinuma <yosukeonrails@gmail.com>
2016-10-31 18:45:30 -0700

    added top tracks with response body

--- servertwo.js
+++ servertwo.js
@@ -1,40 +1,23 @@
-
 var unirest = require('unirest');
 var express = require('express');
 var events = require('events');
 
-var  getRelatedArtist= function(endpoint, id){
-
-  var emitter = new events.EventEmitter();
-  unirest.get('https://api.spotify.com/v1/artists/'+id+'/related-artists')
-  .end(function(response){
-    if (response.ok) {
-
-        emitter.emit('related', response.body);
-        console.log(response.body);
-    }
-    else {
-        emitter.emit('error', response.code);
-    }
-  });
-
-
-};
 
 var getFromApi = function(endpoint, args) {
     var emitter = new events.EventEmitter();
+
     unirest.get('https://api.spotify.com/v1/' + endpoint)
-           .qs(args)
-           .end(function(response) {
-                if (response.ok) {
-                    emitter.emit('end', response.body);
+        .qs(args)
+        .end(function(response) {
+            if (response.ok) {
 
-                }
-                else {
-                    emitter.emit('error', response.code);
-                }
-            });
+                emitter.emit('end', response.body);
 
+
+            } else {
+                emitter.emit('error', response.code);
+            }
+        });
     return emitter;
 };
 
@@ -42,7 +25,6 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/search/:name', function(req, res) {
-
     var searchReq = getFromApi('search', {
         q: req.params.name,
         limit: 1,
@@ -51,23 +33,64 @@ app.get('/search/:name', function(req, res) {
 
 
 
-    searchRel.on('related',function(artistId){
-
-        console.log(artistId);
-
-    });
-
 
     searchReq.on('end', function(item) {
 
         var artist = item.artists.items[0];
-        res.json(artist);
 
-        var artistId = artist.id;
-        var emitter = new events.EventEmitter();
+        var artistId = item.artists.items[0].id;
 
-        var searchRel= getRelatedArtist('artists',artistId);
-        emitter.emit('related', artistId);
+        var searchRel = getFromApi('artists/' + artistId + '/related-artists');
+
+        searchRel.on('end', function(data) {
+
+            artist.related = data.artists;
+
+            var artistArray = data.artists;
+
+
+            artistArray.forEach(function(singleArtist) {
+
+                var singleArtistId = singleArtist.id;
+
+                var searchTop = getFromApi('artists/' + singleArtistId + '/top-tracks', {
+                    country: 'US'
+                }); //request
+
+                searchTop.on('end', function(data) {
+
+                  if(!data){
+                    throw err;
+                  }
+
+
+                });
+
+
+                searchTop.on('error', function(code) {
+                    res.sendStatus(code);
+                });
+
+            });
+
+
+            // artistArray.forEach(function(singleArtist){
+            //     console.log(singleArtist);
+            // });
+            //
+
+            res.json(artist);
+              artist.tracks = data.tracks;
+            console.log(artist);
+
+        });
+
+
+
+        searchRel.on('error', function(code) {
+            res.sendStatus(code);
+        });
+
     });
 
     searchReq.on('error', function(code) {
